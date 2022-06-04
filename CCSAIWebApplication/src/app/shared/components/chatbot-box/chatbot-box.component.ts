cda5cc59721cd84d769b309a18feba0643a5faa2
inyormanjr <ronilojunioryap@gmail.com>
2022-06-04 11:40:15 +1000

    implemented

--- CCSAIWebApplication/src/app/shared/components/chatbot-box/chatbot-box.component.ts
+++ CCSAIWebApplication/src/app/shared/components/chatbot-box/chatbot-box.component.ts
@@ -1,4 +1,7 @@
+import { Message } from './../../models/message';
+import { BotSocketService } from './../../../core/services/socketservice/bot-socket.service';
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chatbot-box',
@@ -6,16 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatbot-box.component.css']
 })
 export class ChatbotBoxComponent implements OnInit {
-  hidden :boolean;
-  constructor() {
+  gotStarted: boolean;
+  hidden: boolean;
+  message: string | undefined;
+  messages: Message[] = [];
+  constructor(private botSocketServce: BotSocketService) {
+    this.gotStarted = false;
     this.hidden = false;
+    botSocketServce.receivedReply().subscribe(x => {
+      if (x) {
+           var newMessage: Message = {
+             SenderMessage: x.outputMessage,
+             ReceiverMessage: '',
+           };
+        setTimeout(() => {
+       this.messages.push(newMessage);
+        }, 3000);
+       }
+
+    })
   }
 
   ngOnInit(): void {
   }
 
+  sendMessage() {
+    if (this.message) {
+    var newMessage: Message = {
+      SenderMessage: '',
+      ReceiverMessage: this.message,
+    };
+    this.messages.push(newMessage);
+      this.botSocketServce.sendMessage(this.message);
+      this.message = '';
+    }
+  }
+
+  getStarted() {
+    this.message = 'Get started';
+    this.gotStarted = true;
+    this.sendMessage();
+  }
+
   showHideBotChat() {
-    console.log(this.hidden)
     if (this.hidden == false) {
         this.hidden = true;
     }
--- CCSAIWebApplication/src/environments/environment.prod.ts
+++ CCSAIWebApplication/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  socket_uri: '',
   api_uri : 'http://localhost:5000/api/v1'
 };
