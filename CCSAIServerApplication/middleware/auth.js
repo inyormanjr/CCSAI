752e27aa4f72ca89065364c9f5f2a9e27fbebc77
Kurt Cal-el C. Ambrosio <calelcambrosio01@gmail.com>
2022-06-12 02:18:51 +0800

    partial fix ui

--- CCSAIServerApplication/middleware/auth.js
+++ CCSAIServerApplication/middleware/auth.js
@@ -6,7 +6,6 @@ const User = require('../models/User');
 exports.protectUser = asyncHandler(async(req, res, next) => {
     let token;
 
-
     if (req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
