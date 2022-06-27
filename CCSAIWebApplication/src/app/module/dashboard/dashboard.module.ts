058810647fc204de51720c35c6a0906cb14ce050
inyormanjr <ronilojunioryap@gmail.com>
2022-06-27 12:34:08 +1000

    additional dashboard components

--- CCSAIWebApplication/src/app/module/dashboard/dashboard.module.ts
+++ CCSAIWebApplication/src/app/module/dashboard/dashboard.module.ts
@@ -5,13 +5,19 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { ProgressLineItemComponent } from './components/progress-line-item/progress-line-item.component';
 import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component';
+import { StudentDashboardComponent } from './components/student-dashboard/student-dashboard.component';
+import { InstructorDashboardComponent } from './components/instructor-dashboard/instructor-dashboard.component';
+import { ActivityCountLineItemComponent } from './components/activity-count-line-item/activity-count-line-item.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressLineItemComponent,
-    DashboardCardComponent
+    DashboardCardComponent,
+    StudentDashboardComponent,
+    InstructorDashboardComponent,
+    ActivityCountLineItemComponent
   ],
   imports: [
     CommonModule,
