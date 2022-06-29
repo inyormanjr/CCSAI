37e8134e847a70eeeda5b8104fce3dd5dc95ec9a
Kurt Cal-el C. Ambrosio <calelcambrosio01@gmail.com>
2022-06-29 19:00:28 +0800

    update aggregate

--- CCSAIServerApplication/controllers/dashboard.js
+++ CCSAIServerApplication/controllers/dashboard.js
@@ -1,120 +1,118 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const enrollmentDetail = require('../models/EnrollmentDetail');
-var ObjectId = require('mongodb').ObjectId; 
+var ObjectId = require('mongodb').ObjectId;
 
 
-exports.getCourseProgressByStudent = asyncHandler(async (req, res, next) => {
-    let studentId = req.params.id;
+exports.getCourseProgressByStudent = asyncHandler(async(req, res, next) => {
+    //let studentId = req.params.id;
     var obid = new ObjectId('62a465162e8f9724eb8342c6');
-    result = await enrollmentDetail.aggregate([
-      {
-        $lookup: {
-          from: 'enrollments',
-          localField: 'enrollmentId',
-          foreignField: '_id',
-          as: 'enrollments',
+    var studentId = new ObjectId('629cbb5d79fb9eb8bbeade07');
+    result = await enrollmentDetail.aggregate([{
+            $lookup: {
+                from: 'enrollments',
+                localField: 'enrollmentId',
+                foreignField: '_id',
+                as: 'enrollments',
+            },
         },
-      },
-      {
-        $lookup: {
-          from: 'courses',
-          localField: 'enrollments.courseId',
-          foreignField: '_id',
-          as: 'courses',
+        {
+            $lookup: {
+                from: 'courses',
+                localField: 'enrollments.courseId',
+                foreignField: '_id',
+                as: 'courses',
+            },
         },
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'enrollments.instructorId',
-          foreignField: '_id',
-          as: 'instructor',
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'enrollments.instructorId',
+                foreignField: '_id',
+                as: 'instructor',
+            },
         },
-      },
 
-      {
-        $project: {
-          courses: 1,
-          instructor: 1,
+        {
+            $match: { 'enrollments.termId': obid, studentId },
         },
-      },
+        {
+            $project: {
+                courses: 1,
+                instructor: 1,
+            },
+        },
 
-      {
-        $match: { 'termId': obid },
-      },
     ]);
     res.status(200).json(result);
 });
 
-exports.getExercisesProgressByStudent = asyncHandler(async (req, res, next) => {
+exports.getExercisesProgressByStudent = asyncHandler(async(req, res, next) => {
     res.status(200).json({
-      success: true,
-      data: [
-        {
-          courseCode: 'CS101',
-          courseName: 'Basic Programming',
-          progressValue: 4,
-          maxValue: 10,
-        },
-        {
-          courseCode: 'CS101',
-          courseName: 'Basic Programming',
-          progressValue: 2,
-          maxValue: 10,
-        },
-        {
-          courseCode: 'CS101',
-          courseName: 'Basic Programming',
-          progressValue: 5,
-          maxValue: 10,
-        },
-      ],
+        success: true,
+        data: [{
+                courseCode: 'CS101',
+                courseName: 'Basic Programming',
+                progressValue: 4,
+                maxValue: 10,
+            },
+            {
+                courseCode: 'CS101',
+                courseName: 'Basic Programming',
+                progressValue: 2,
+                maxValue: 10,
+            },
+            {
+                courseCode: 'CS101',
+                courseName: 'Basic Programming',
+                progressValue: 5,
+                maxValue: 10,
+            },
+        ],
     });
 
 });
 
 exports.getAssessmentsProgressByStudent = asyncHandler(
-  async (req, res, next) => {
+    async(req, res, next) => {
         let studentId = req.params.id;
-         res.status(200).json({
-           success: true,
-           data: [
-             {
-               courseCode: 'CS101',
-               courseName: 'Basic Programming',
-               progressValue: 2,
-               maxValue: 10,
-             },
-             {
-               courseCode: 'CS101',
-               courseName: 'Basic Programming',
-               progressValue: 2,
-               maxValue: 10,
-             },
-             {
-               courseCode: 'CS101',
-               courseName: 'Basic Programming',
-               progressValue: 7,
-               maxValue: 10,
-             },
-           ],
-         });
-  }
+        res.status(200).json({
+            success: true,
+            data: [{
+                    courseCode: 'CS101',
+                    courseName: 'Basic Programming',
+                    progressValue: 2,
+                    maxValue: 10,
+                },
+                {
+                    courseCode: 'CS101',
+                    courseName: 'Basic Programming',
+                    progressValue: 2,
+                    maxValue: 10,
+                },
+                {
+                    courseCode: 'CS101',
+                    courseName: 'Basic Programming',
+                    progressValue: 7,
+                    maxValue: 10,
+                },
+            ],
+        });
+    }
 );
 
 exports.getCoursesByUserByCurrentActiveTerm = asyncHandler(
-  async (req, res, next) => {
-    let studentId = req.params.id;
-  }
+    async(req, res, next) => {
+        let studentId = req.params.id;
+    }
 );
 
 exports.getAnnouncements = asyncHandler(
-  async (req, res, next) => {
-    let studentId = req.params.id;
-  }
+    async(req, res, next) => {
+        let studentId = req.params.id;
+    }
 );
 
-exports.getRecentGradeUpdates = asyncHandler(async (req, res, next) => {
-  let studentId = req.params.id;
+exports.getRecentGradeUpdates = asyncHandler(async(req, res, next) => {
+    let studentId = req.params.id;
 });
