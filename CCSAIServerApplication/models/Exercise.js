1626a5fcf0d18cde3e6afc98dc10516eeabd2a55
inyormanjr <ronilojunioryap@gmail.com>
2022-08-14 01:22:25 +1000

    exerciseName, instruction, status

--- CCSAIServerApplication/models/Exercise.js
+++ CCSAIServerApplication/models/Exercise.js
@@ -1,18 +1,26 @@
 const mongoose = require('mongoose');
 
 const ExerciseSchema = new mongoose.Schema({
-    discussionId: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Discussion',
-        required: true
+  discussionId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Discussion',
+    required: true,
+  },
+  exerciseName: { type: String, required: true },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  exercise_status : { type: String, required: true , default : "active" },
+  exerciseDetails: {
+    type: String,
+    required: true,
+  },
+  answers: [
+    {
+      type: String,
     },
-    exerciseDetails: {
-        type: String,
-        required: true
-    },
-    answers: [{
-        type: String
-    }]
+  ],
 });
 
 module.exports = mongoose.model('Exercise', ExerciseSchema);
